54f1b0b4576ec57f310efbafcd5d561613be4fa2
gStudentGit <dffshem@gmail.com>
2026-05-28 20:18:44 +0300

    Added __init__, now we can do: from campaign_post_scraper import run_pipeline, parse_campaign_input, BrightDataClient

--- campaign_post_scraper/__init__.py
+++ campaign_post_scraper/__init__.py
@@ -1,1 +1,18 @@
 """Campaign Post Scraper - A pipeline for collecting social media posts from X via BrightData."""
+
+from campaign_post_scraper.input_parser import parse_campaign_input
+from campaign_post_scraper.query_constructor import build_queries
+from campaign_post_scraper.brightdata_client import BrightDataClient, RateLimiter
+from campaign_post_scraper.post_mapper import map_posts
+from campaign_post_scraper.json_exporter import export_json
+from campaign_post_scraper.pipeline import run_pipeline
+
+__all__ = [
+    "parse_campaign_input",
+    "build_queries",
+    "BrightDataClient",
+    "RateLimiter",
+    "map_posts",
+    "export_json",
+    "run_pipeline",
+]
